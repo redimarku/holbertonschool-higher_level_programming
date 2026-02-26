3fd8d97ad3fdc383bb028cec3e34108c54602c80
12785@holbertonstudents.com <you@example.com>
2026-02-26 18:57:07 +0100

    Initial commit

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,1 +1,13 @@
 -- script to list all databases
+
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table(id, name, score) VALUES (4, "George", 8),(2, "Alex", 3),(3, "Bob", 14),(4, "George", 8);
+
+-- INSERT INTO second_table(id, name, score) VALUES (1, "John", 10);
+-- INSERT INTO second_table(id, name, score) VALUES (2, "Alex", 3);
+-- INSERT INTO second_table(id, name, score) VALUES (3, "Bob", 14);
+-- INSERT INTO second_table(id, name, score) VALUES (4, "George", 8);
+
+
+
+
