1dd62384ef356a83821e0edeeb837298d528eae4
12785@holbertonstudents.com <you@example.com>
2026-02-26 23:57:03 +0100

    Initial commit

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,7 @@
 -- script to list all databases
 
+-- Show all privileges for user user_0d_1 connected from localhost
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Show all privileges for user user_0d_2 connected from localhost
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
