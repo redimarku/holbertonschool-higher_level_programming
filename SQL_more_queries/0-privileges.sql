6c410bb0dc8c5d7f0b1366be794e624348a29d16
12785@holbertonstudents.com <you@example.com>
2026-02-26 23:56:33 +0100

    Initial commit

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,11 +1,4 @@
--- write a script that print all privileges of user : user_0d_1
--- create users 
-CREATE USER 'user_0d_1'@'localhost';
-CREATE USER 'user_0d_2'@'localhost';
-
--- grant users privileges
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- script to list all databases
 
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
--- write a script that print all privileges of user : user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
