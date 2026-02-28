2c8b26683727dd0d424ddb398d653cb2532b22a8
12785@holbertonstudents.com <you@example.com>
2026-02-28 18:44:04 +0100

    Initial commit

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,4 +1,5 @@
--- script to list all databases
+-- creates the table users with constraints
+
 CREATE TABLE IF NOT EXISTS users (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   email VARCHAR(255) NOT NULL UNIQUE,
