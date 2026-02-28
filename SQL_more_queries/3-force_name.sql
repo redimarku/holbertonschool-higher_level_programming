3ff4eade7f9faf337aaa2f593047844430069cb2
12785@holbertonstudents.com <you@example.com>
2026-02-28 15:29:21 +0100

    Initial commit

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,2 +1,2 @@
 --write a script that create a table force_name
-CREATE TABLE IF NOT EXISTS force_name (id int,name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS force_name (id INT ,name VARCHAR(256));
